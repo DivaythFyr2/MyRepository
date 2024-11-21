0848aba1c1d1bf2e394113eb1d5dd4a913dd9a56
vlad-vladik-afanasev@mail.ru <vlad-vladik-afanasev@mail.ru>
2024-11-21 23:24:41 +0300

    Test commit add solution

--- src/javarush/com/Solution.java
+++ src/javarush/com/Solution.java
@@ -3,11 +3,11 @@ package javarush.com;
 public class Solution {
 
     public static void main(String[] args) {
-        int[][] table = new int[10][10];
-        for (int i = 0; i <table.length; i++) {
-            for (int j = 0; j <table[i].length; j++) {
-                table[i][j] = (i+1)*(j+1);
-                System.out.print(table[i][j] + " ");
+        int[][] multiplicationTable = new int[10][10];
+        for (int i = 0; i <multiplicationTable.length; i++) {
+            for (int j = 0; j <multiplicationTable[i].length; j++) {
+                multiplicationTable[i][j] = (i+1)*(j+1);
+                System.out.print(multiplicationTable[i][j] + " ");
             }
             System.out.println();
         }
